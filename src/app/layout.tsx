1dba8857737dbb2418fbcd89e97c1079ff2a7aff
Pelle Lundgren <pellburt@gmail.com>
2026-08-18 20:21:01 +0200

    feat(layout): wrap children in section that handle responsivness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     >
       <body className="min-h-full flex flex-col">
         <Header />
-        {children}
+        <section className="flex-1 flex flex-col font-sans items-center justify-items-center p-8 gap-4">
+          {children}
+        </section>
         <Footer />
       </body>
     </html>
