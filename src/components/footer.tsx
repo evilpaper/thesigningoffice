59b51a26e889bf5d59cf8d1c3052890133e53be7
Pelle Lundgren <pellburt@gmail.com>
2026-08-19 07:23:21 +0200

    fix: replace arbitrary value in class gap-[24px] with Tailwind default spacing scale gap-6

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-full flex gap-[24px] flex-wrap items-center justify-between p-8">
+    <footer className="w-full flex gap-6 flex-wrap items-center justify-between p-8">
       <Link href="/privacy" className="text-sm">
         Privacy
       </Link>
