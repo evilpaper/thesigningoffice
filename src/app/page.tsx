aad1f68d73cc816ca053fd92e036120b5800e3da
Pelle Lundgren <pellburt@gmail.com>
2026-09-05 07:53:14 +0200

    refactor: rename start signing section to page to remvoe DOM connection and stay more true to the domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import StartSigningSection from "@/features/signing/start-signing-section";
+import StartSigningPage from "@/features/signing/start-signing-page";
 
 export default function Home() {
   return (
     <main className="flex flex-1 w-full max-w-7xl mx-auto flex-col lg:flex-row lg:items-start gap-12 lg:gap-16 px-8">
-      <StartSigningSection />
+      <StartSigningPage />
     </main>
   );
 }
